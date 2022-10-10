a5a96178aed462249102f07b98b3f4d20c95a71e
David Ankin <daveankin@gmail.com>
2022-10-10 07:21:49 -0400

    add lombok sneaky throws reproducer code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+import lombok.SneakyThrows;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            throwsSomething();
+        } catch (Exception e) {
+            System.out.println("caught!");
+        }
+    }
+
+    @SneakyThrows
+    static void throwsSomething() {
+        throw new Exception();
     }
 }
